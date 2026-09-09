3137f68318ddd5416c0c1370415ea2dc939cf471
isaku862 <isaku862@gmail.com>
2026-09-10 02:09:00 +0900

    step2.checkstyle修正版

---
+++ src/main/java/org/example/Trade.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.time.LocalDateTime;
+
+public class Trade {
+
+    private LocalDateTime tradedDateTime;
+    private String symbol;
+    private String side;
+    private long quantity;
+    private double unitPrice;
+
+    public Trade(LocalDateTime tradedDateTime, String symbol,
+                 String side, long quantity, double unitPrice) {
+
+        this.tradedDateTime = tradedDateTime;
+        this.symbol = symbol;
+        this.side = side;
+        this.quantity = quantity;
+        this.unitPrice = unitPrice;
+    }
+
+    public LocalDateTime getTradedDateTime() {
+        return tradedDateTime;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public String getSide() {
+        return side;
+    }
+
+    public long getQuantity() {
+        return quantity;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+}
